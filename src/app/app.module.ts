89fc56d9a7d7d62e54536744dc0fd44cdd8baac1
ABOUSAID Salah-Eddine <abousaid7salah@gmail.com>
2020-05-14 11:37:15 +0000

    create some componenets and work with Routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,15 +7,32 @@ import { MatListModule } from '@angular/material/list';
 import { MatGridListModule } from '@angular/material/grid-list';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
+import 'hammerjs';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
-import 'hammerjs';
 import { MenuComponent } from './menu/menu.component';
+import { DishDetailComponent } from './dish-detail/dish-detail.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+
+import { DishService } from './services/dish.service';
 
 @NgModule({
-  declarations: [AppComponent, MenuComponent],
+  declarations: [
+    AppComponent,
+    MenuComponent,
+    DishDetailComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    AboutComponent,
+    ContactComponent,
+  ],
   imports: [
     BrowserModule,
     AppRoutingModule,
@@ -27,7 +44,7 @@ import { MenuComponent } from './menu/menu.component';
     MatCardModule,
     MatButtonModule,
   ],
-  providers: [],
+  providers: [DishService],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
---
+++ src/app/router/routes.ts
@@ -0,0 +1,17 @@
+import { Routes } from '@angular/router';
+
+import { MenuComponent } from '../menu/menu.component';
+import { DishDetailComponent } from '../dish-detail/dish-detail.component';
+import { HeaderComponent } from '../header/header.component';
+import { FooterComponent } from '../footer/footer.component';
+import { HomeComponent } from '../home/home.component';
+import { AboutComponent } from '../about/about.component';
+import { ContactComponent } from '../contact/contact.component';
+
+export const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'menu', component: MenuComponent },
+  { path: 'contact', component: ContactComponent },
+];
