2601641af6b2d3586d08c5c9475444521ff2043a
ABOUSAID Salah-Eddine <abousaid7salah@gmail.com>
2020-05-17 16:26:28 +0000

    Comment

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,10 +7,12 @@ import { MatListModule } from '@angular/material/list';
 import { MatGridListModule } from '@angular/material/grid-list';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
+
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatSliderModule } from '@angular/material/slider';
 import { MatSelectModule } from '@angular/material/select';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
@@ -64,6 +66,7 @@ import { LoginComponent } from './login/login.component';
     MatSelectModule,
     MatSlideToggleModule,
     MatProgressSpinnerModule,
+    MatSliderModule,
   ],
   providers: [DishService, PromotionService, LeaderService],
   entryComponents: [LoginComponent],
--- src/app/dish-detail/dish-detail.component.ts
+++ src/app/dish-detail/dish-detail.component.ts
@@ -1,9 +1,11 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, ViewChild } from '@angular/core';
 import { Params, ActivatedRoute } from '@angular/router';
 import { Location } from '@angular/common';
 import { Dish } from '../shared/dish';
+import { Comment } from '../shared/Comment';
 import { DishService } from '../services/dish.service';
 import { switchMap } from 'rxjs/operators';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-dish-detail',
@@ -17,11 +19,41 @@ export class DishDetailComponent implements OnInit {
   prev: string;
   next: string;
 
+  dishCopy: Dish = null;
+  rating: number;
+  newComment: Comment;
+  commentForm: FormGroup;
+  errMess: string;
+  visibility = 'shown';
+
+  formErrors = {
+    name: '',
+    message: '',
+  };
+
+  validationMessages = {
+    name: {
+      required: 'Name is required.',
+      minlength: 'Name must be at least 2 characters long.',
+      maxlength: 'Name cannot be more than 25 characters long.',
+    },
+    message: {
+      required: 'Comment is required.',
+    },
+  };
+
   constructor(
     private dishService: DishService,
     private route: ActivatedRoute,
-    private location: Location
-  ) {}
+    private location: Location,
+    private fb: FormBuilder
+  ) {
+    this.createForm();
+  }
+
+  //Author Name must be at least two characters long.
+  //Author Name is required
+  //comment as required
 
   ngOnInit(): void {
     // let id = this.route.snapshot.params['id'];
@@ -36,6 +68,7 @@ export class DishDetailComponent implements OnInit {
       )
       .subscribe((dish) => {
         this.dish = dish;
+        this.dishCopy = dish;
         this.setPrevNext(dish.id);
       });
   }
@@ -52,5 +85,64 @@ export class DishDetailComponent implements OnInit {
 
   getBack(): void {
     this.location.back();
+  }
+
+  createForm() {
+    this.commentForm = this.fb.group({
+      name: [
+        '',
+        [
+          Validators.required,
+          Validators.minLength(2),
+          Validators.maxLength(25),
+        ],
+      ],
+      message: ['', Validators.required],
+    });
+
+    this.commentForm.valueChanges.subscribe((data) =>
+      this.onValueChanges(data)
+    );
+
+    this.onValueChanges();
+  }
+
+  onValueChanges(data?: any) {
+    if (!this.commentForm) {
+      return;
+    }
+
+    const form = this.commentForm;
+
+    for (const field in this.formErrors) {
+      this.formErrors[field] = '';
+      const control = form.get(field);
+      if (control && control.dirty && !control.valid) {
+        const messages = this.validationMessages[field];
+        for (const key in control.errors) {
+          this.formErrors[field] += messages[key] + ' ';
+        }
+      }
+    }
+  }
+
+  onChangeRatingSlider(slider): void {
+    this.rating = slider.value;
+  }
+
+  onSubmit(): void {
+    let comment = this.commentForm.value;
+
+    this.newComment = {
+      rating: this.rating,
+      comment: comment.message,
+      author: comment.name,
+      date: new Date().toISOString(),
+    };
+    console.log(this.newComment);
+
+    this.dish.comments.push(this.newComment);
+
+    this.commentForm.reset();
   }
 }
